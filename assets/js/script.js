cbb455d0e7fc5fe99d83f9dcc75bd53aeff77b95
Matt <matt@pandolfo.com>
2021-08-11 14:35:01 -0400

    add dark mod toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sidebar");
-
-btn.onclick = function() {
-  sidebar.classList.toggle("active");
-}
+let arrow = document.querySelectorAll(".arrow");
+      for (var i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener("click", (e) => {
+          let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+          arrowParent.classList.toggle("showMenu");
+        });
+      }
+      let sidebar = document.querySelector(".sidebar");
+      let sidebarBtn = document.querySelector(".bx-menu");
+      console.log(sidebarBtn);
+      sidebarBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+      });
